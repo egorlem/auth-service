d4f6f93f3447658acf9a22be1a5d91f9f249dfb4
guesswhozzz <torridtime@gmail.com>
2022-10-16 15:11:30 +0300

    Update deps

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('ok?');
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('error', (err) => console.log('Redis Client Error', err));
+
+await client.connect();
+
+await client.set('key2', 'value gogogogo');
+const value = await client.get('key2');
+
+console.log(value);
